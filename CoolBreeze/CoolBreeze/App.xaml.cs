d020c4af85520736dbadda6f87d934743c86a96d
n0f3 <n0f3@outlook.com>
2017-03-28 22:42:14 -0700

    Finished part 2 of the challenge

--- CoolBreeze/CoolBreeze/App.xaml.cs
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -15,7 +15,7 @@ namespace CoolBreeze
         {
             InitializeComponent();
 
-            MainPage = new CoolBreeze.MainPage();
+            MainPage = new NavigationPage(new StartPage());
         }
 
         protected override void OnStart()
